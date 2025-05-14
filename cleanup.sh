13a0b5a45e3005c4c9b6bcad9b7527cb52945bd6
sunsetblvdtunehaumea <fitnessegggoodfellas@gmail.com>
2025-05-14 14:56:11 -0400

    Initial commit 3

--- cleanup.sh
+++ cleanup.sh
@@ -4,10 +4,12 @@
 rm -f /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/api/HelloWorldApi.java
 rm -f /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/model/HelloResponse.java
 rm -f /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/model/InfoResponse.java
+rm -f /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/config/WebConfig.java
 
 # Remove empty directories (these will be created automatically by the OpenAPI generator during build)
 rmdir -p /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/model/gen
 rmdir -p /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/api/gen
+rmdir -p /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/config
 
 # Remove the entire model directory if it exists
 rm -rf /Users/skramasamy/Documents/coding/AI_Training_Balaji/playground/spring-boot-app/src/main/java/com/example/demo/model
